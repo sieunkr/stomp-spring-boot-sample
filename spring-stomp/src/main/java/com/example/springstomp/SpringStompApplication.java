1841f48acb54950c38b903cea858368ab257f875
sieunkim <sieunkr@gmail.com>
2022-01-21 20:16:07 +0900

    프로젝트 초기 셋팅

---
+++ spring-stomp/src/main/java/com/example/springstomp/SpringStompApplication.java
@@ -0,0 +1,13 @@
+package com.example.springstomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStompApplication.class, args);
+    }
+
+}

017068f73d999f289789b6c6c26aaadf7fdeaa4a
Eddy.Kim <sieunkr@gmail.com>
2022-01-22 15:45:17 +0900

    Feature/stomp rabbitmq (#3) (#4)
    * stomp-rabbitmq 프로젝트 셋팅
    
    * rabbitmq 연동

---
+++ spring-stomp-rabbitmq/src/test/java/com/example/springstomprabbitmq/SpringStompRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstomprabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStompRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
